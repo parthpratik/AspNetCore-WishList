bf8d57b3f43b1fa11548271b022110b2eabe64a0
parthpratik <84229082+parthpratik@users.noreply.github.com>
2022-04-15 14:21:25 +0530

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -17,6 +17,7 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +25,16 @@ namespace WishList
         {
             app.Run(async (context) =>
             {
+                if(env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                }
+                else
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                }
+                app.UseRouting();
+                app.UseEndpoints(endpoints =>{endpoints.MapDefaultControllerRoute();});
                 await context.Response.WriteAsync("Hello World!");
             });
         }
